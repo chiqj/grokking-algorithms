18ba3a45cfb81db0b1ca3ab38d5fcbd205652b09
MrChi <chiqingjun@gmail.com>
2019-05-06 21:22:56 +0800

    add in-place algorithm to quick sort

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -19,3 +19,30 @@ class QuickSort:
                     right_partition.append(i)
             return self.sort(left_partition) + [pivot] \
                 + self.sort(right_partition)
+
+
+class QuickSortInPlace:
+    def partition(self, array: list, left: int, right: int):
+        if left >= right:
+            return
+        else:
+            # choose pivot
+            pivot_idx = random.randint(left, right)
+            pivot = array[pivot_idx]
+            # swap pivot to the end
+            array[pivot_idx], array[right] = array[right], array[pivot_idx]
+            store_idx = left
+            # if value is smaller than pivot, swap it to the front
+            for i in range(left, right):
+                if array[i] < pivot:
+                    array[i], array[store_idx] = array[store_idx], array[i]
+                    store_idx += 1
+            # swap pivot to the store_idx position
+            array[store_idx], array[right] = array[right], array[store_idx]
+
+            self.partition(array, left, store_idx-1)
+            self.partition(array, store_idx+1, right)
+
+    def sort(self, array: list) -> list:
+        self.partition(array, 0, len(array)-1)
+        return array
--- tests/test_quick_sort.py
+++ tests/test_quick_sort.py
@@ -2,12 +2,36 @@
 
 import pytest
 
-from algorithms.quick_sort import QuickSort
+from algorithms.quick_sort import QuickSort, QuickSortInPlace
 
 
-@pytest.mark.parametrize('array', ([3, 1, 4, 1, 5, 9], [], [10], [3, 1]))
+@pytest.mark.parametrize(
+    'array',
+    (
+        [3, 1, 4, 1, 5, 9, 2, 5, 4],
+        [],
+        [10],
+        [3, 1],
+        [1, 2, 2, 3],
+    )
+)
 def test_quick_sort(array):
     obj = QuickSort()
-    array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
     sorted_array = sorted(array)
     assert obj.sort(array) == sorted_array
+
+
+@pytest.mark.parametrize(
+    'array',
+    (
+        [3, 1, 4, 1, 5, 9, 2, 5, 4],
+        [],
+        [10],
+        [3, 1],
+        [1, 2, 2, 3]
+    )
+)
+def test_quick_sort_in_place(array):
+    obj = QuickSortInPlace()
+    obj.sort(array)
+    assert array == sorted(array)
